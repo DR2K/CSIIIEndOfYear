b640ffb4716f852c99983c4b859f66c9918cf5e3
Dev <devdeeprajpal2k@outlook.com>
2019-04-05 09:32:39 -0500

    /

---
+++ src/Encryptor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Encryptor {
+    public static void main(String[]args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Value to encrypt? : ");
+        String value = in.next();
+
+    }
+}
